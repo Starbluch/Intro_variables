a4ecf50afa00e8329fb4629d3c44018237f286d9
admin <gadzukoleksandr29@gmail.com>
2024-11-21 00:04:51 +0200

    lab ex_3

--- main.cpp
+++ main.cpp
@@ -10,8 +10,9 @@ using namespace std;
 int main() {
     cout << char(201) << string(60, (char)205) << char(187) << endl;
     cout << char(186) << "\t\t\t   Vremena goda\t\t\t     " << char(186) << endl;
-    cout << char(204) << string(20, (char)205) << char(203) << string(20, (char)205) << char(203) << string(18, (char)205) << char(182) << endl;
-
+    cout << char(204) << string(15, (char)205) << char(203) << string(16, (char)205) << char(203) << string(14, (char)205) << char(203) << string(12, (char)205)<< char(182) << endl;
+    cout << char(186) << "      Zima     " << char(186) << "      Vesna     " << char(186) << "     Leto     " << char(186) << "    Osen    "<< char(186) << endl;
+    cout << char(200) << string(15, (char)205) << char(202) << string(16, (char)205) << char(202) << string(14, (char)205) << char(202) << string(12, (char)205)<< char(188) << endl;
 //    cout<<"\n"<<(char)201<<(char)205<<(char)205<<(char)205<<(char)187 <<
 
     return 0;
