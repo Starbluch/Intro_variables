eb7017127c86504782d547fb4b0531a4fbdf35d6
admin <gadzukoleksandr29@gmail.com>
2024-11-20 23:44:02 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+#include <iomanip>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cout << char(201) << string(60, (char)205) << char(187) << endl;
+    cout << char(186) << "\t\t\t   Vremena goda\t\t\t     " << char(186) << endl;
+    cout << char(204) << string(20, (char)205) << char(203) << string(20, (char)205) << char(203) << string(18, (char)205) << char(182) << endl;
+
+//    cout<<"\n"<<(char)201<<(char)205<<(char)205<<(char)205<<(char)187 <<
+
+    return 0;
+}
